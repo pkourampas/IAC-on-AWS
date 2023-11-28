7dc0197ef93f2ada4407fa2f4cc386fa440f912b
Petros Kourampas <pkourampas@gmail.com>
2023-11-28 23:16:20 +0200

    fix variables

--- Infrastructure/vpc/main.tf
+++ Infrastructure/vpc/main.tf
@@ -33,4 +33,15 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+# Public Subnet Config
+resource "aws_subnet" "public-subnet" {
+  vpc_id                  = aws_vpc.main-vpc.id
+  cidr_block              = cidrsubnet(var.aws_vpc_cidr, 8)
+  map_public_ip_on_launch = true
+  availability_zone       = "${var.aws_region}a"
+
+  tags = {
+    "Name" = "public-subnet-1"
+  }
+}
 
